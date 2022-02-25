e68288909389ab3feeffb64ff8e7a5d558cf4ed3
doyu <ydh0905@nate.com>
2022-02-25 21:14:58 +0900

    JS: navbar toggle button

--- main.js
+++ main.js
@@ -20,7 +20,14 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 // handle click on "contact me"
